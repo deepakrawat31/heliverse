2e585bcb2f33a5ee2c9d889813b2cec7b284fe9a
Deepak Rawat <deepakrawat.mb@gmail.com>
2024-05-21 11:53:07 +0530

    animation tweaks

--- src/components/Animation.jsx
+++ src/components/Animation.jsx
@@ -10,7 +10,7 @@ export default function Animation() {
          gsap.to(".circle", {
             opacity: 0,
             duration: 1.5,
-            stagger: 0.2,
+            stagger: 0.4,
             ease: "sine.inOut",
          });
       },
@@ -22,10 +22,10 @@ export default function Animation() {
          className="absolute -z-20 top-0 left-0 right-0 h-dvh"
          ref={container}
       >
-         <span className="circle absolute top-20 md:top-40 left-6 md:left-20 aspect-square h-20 md:h-40 rounded-full bg-rose-400/40 blur-2xl block"></span>
-         <span className="circle absolute top-52 md:top-72 left-52 md:left-96 aspect-square h-32 md:h-60 rounded-full bg-amber-400/40 blur-2xl block"></span>
-         <span className="circle absolute bottom-10 right-14 md:right-40 aspect-square h-12 md:h-28 rounded-full bg-emerald-400/40 blur-2xl block"></span>
-         <span className="circle absolute top-40 md:top-10 right-40 md:right-60 aspect-square h-60 md:h-96 rounded-full bg-purple-400/40 blur-2xl block"></span>
+         <span className="circle absolute top-20 md:top-40 left-14 md:left-20 aspect-square h-20 md:h-40 rounded-full bg-rose-400/40 blur-2xl block"></span>
+         <span className="circle absolute top-44 md:top-72 left-72 md:left-96 aspect-square h-32 md:h-60 rounded-full bg-amber-400/40 blur-2xl block"></span>
+         <span className="circle absolute bottom-24 md:bottom-10 right-10 md:right-40 aspect-square h-20 md:h-28 rounded-full bg-emerald-400/40 blur-2xl block"></span>
+         <span className="circle absolute top-80 md:top-10 right-52 md:right-60 aspect-square h-60 md:h-96 rounded-full bg-purple-400/40 blur-2xl block"></span>
       </div>
    );
 }
