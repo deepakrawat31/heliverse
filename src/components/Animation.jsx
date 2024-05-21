16b3b9cc0cff703bdc00d19708bcb66e8ac405c3
Deepak Rawat <deepakrawat.mb@gmail.com>
2024-05-21 11:41:44 +0530

    minor update

--- src/components/Animation.jsx
+++ src/components/Animation.jsx
@@ -22,10 +22,10 @@ export default function Animation() {
          className="absolute -z-20 top-0 left-0 right-0 h-dvh"
          ref={container}
       >
-         <span className="circle absolute top-20 md:top-40 left-6 md:left-20 aspect-square h-40 rounded-full bg-rose-400/40 blur-2xl block"></span>
-         <span className="circle absolute top-40 md:top-72 left-36 md:left-96 aspect-square h-60 rounded-full bg-amber-400/40 blur-2xl block"></span>
-         <span className="circle absolute bottom-10 right-14 md:right-40 aspect-square h-28 rounded-full bg-emerald-400/40 blur-2xl block"></span>
-         <span className="circle absolute top-10 right-28 md:right-60 aspect-square h-96 rounded-full bg-purple-400/40 blur-2xl block"></span>
+         <span className="circle absolute top-20 md:top-40 left-6 md:left-20 aspect-square h-20 md:h-40 rounded-full bg-rose-400/40 blur-2xl block"></span>
+         <span className="circle absolute top-40 md:top-72 left-36 md:left-96 aspect-square h-32 md:h-60 rounded-full bg-amber-400/40 blur-2xl block"></span>
+         <span className="circle absolute bottom-10 right-14 md:right-40 aspect-square h-12 md:h-28 rounded-full bg-emerald-400/40 blur-2xl block"></span>
+         <span className="circle absolute top-10 right-28 md:right-60 aspect-square h-60 md:h-96 rounded-full bg-purple-400/40 blur-2xl block"></span>
       </div>
    );
 }
